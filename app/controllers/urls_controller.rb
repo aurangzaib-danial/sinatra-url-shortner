9a07b8d319b836000c7b55fba2aaa5d225ea19f2
Aurangzaib <aurangzaib.danial@gmail.com>
2019-12-11 16:57:48 +0500

    refactored post route

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -12,29 +12,30 @@ class UrlsController < ApplicationController
 
 	post '/urls' do
 		
-		if Url.valid_target_url?(params[:target_url])
-			
-			if logged_in?
-				url = Url.find_or_create_by(target_url: params[:target_url], user_id: session[:user_id])
-			else
-				url = Url.new(target_url: params[:target_url])
-
-				url_hash = temporary_urls.detect {|url_hash| url_hash[:target_url] == params[:target_url]}
-
-				if url_hash
-					url.id = url_hash[:id]
-				else
-					url.save
-					temporary_urls << { id: url.id, target_url: url.target_url}
-				end
-					
-			end
-			
-			flash[:shortened_url] = url.shorten(host)
-	  else
-	  	flash[:error] = "Unable to shorten that link. It is not a valid url."
+		unless Url.valid_target_url?(params[:target_url])
+			flash[:error] = "Unable to shorten that link. It is not a valid url."
+			redirect '/'
 		end
-	  redirect '/'
+			
+		if logged_in?
+			url = Url.find_or_create_by(target_url: params[:target_url], user_id: session[:user_id])
+		else
+			url = Url.new(target_url: params[:target_url])
+
+			url_hash = temporary_urls.detect {|url_hash| url_hash[:target_url] == params[:target_url]}
+
+			if url_hash
+				url.id = url_hash[:id]
+			else
+				url.save
+				temporary_urls << { id: url.id, target_url: url.target_url}
+			end
+				
+		end
+		
+		flash[:shortened_url] = url.shorten(host)
+	
+		redirect '/'
   end
 
 end
